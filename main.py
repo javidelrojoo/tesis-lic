6cf30c36580b8946643eecec73a5d0b166a400b6
javidelrojoo <javitau459@gmail.com>
2026-02-19 12:10:31 -0300

    Completo el main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
+from SkeletonAnalysis import SkeletonAnalysis
 
 muestra = 'M3'
-dir_path = './Muestras/' + muestra + '/'
+dir_path = './muestras/' + muestra + '/'
+img_path = dir_path + 'M3_binary.tif'
+electrodes_path = (dir_path + 'M3_electrodo_mask1.JPG', dir_path + 'M3_electrodo_mask2.JPG')
 
+SA = SkeletonAnalysis(img_path)
+
+SA.complete_analysis(electrodes_path=electrodes_path, plot=True, verbose=True)
