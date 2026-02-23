d59eaaa73de0205e7bda712c6200d60e199c324d
javidelrojoo <javitau459@gmail.com>
2026-02-23 14:50:27 -0300

    Agrego un node merger que era lo que originalmente queria

--- temp.py
+++ temp.py
@@ -4,7 +4,23 @@ import itertools
 import matplotlib.pyplot as plt
 
 path = 'muestras/M3/M30007.graphml'
+#%% Node Merger
+G = nx.Graph(nx.read_graphml(path))
 
+edges = list(G.edges())
+np.random.shuffle(edges)
+merge_prob = 0.01
+
+print(f"{G.number_of_nodes()} nodos inciales.")
+count_merges = 0
+for u, v in edges:
+    if G.has_node(u) and G.has_node(v):    
+        if np.random.rand() < merge_prob:
+            nx.contracted_nodes(G, u, v, self_loops=False, copy=False)
+            count_merges += 1
+            
+print(f"{G.number_of_nodes()} nodos restantes. Se realizaron {count_merges} fusiones.")
+#%% Edge Merger
 G = nx.Graph(nx.read_graphml(path))
 pos = nx.spring_layout(G, k=1/np.sqrt(G.number_of_nodes()), seed=10)
 
