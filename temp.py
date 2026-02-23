066f5b94289f04ed8a2e4cb32d9dc6f4948754fa
javidelrojoo <javitau459@gmail.com>
2026-02-23 11:47:27 -0300

    Termino el codigo para juntar nodos

--- temp.py
+++ temp.py
@@ -1,13 +1,36 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 import numpy as np
+import itertools
+import matplotlib.pyplot as plt
 
 path = 'muestras/M3/M30007.graphml'
 
 G = nx.Graph(nx.read_graphml(path))
+pos = nx.spring_layout(G, k=1/np.sqrt(G.number_of_nodes()), seed=10)
 
-ohmic_ratio = 0.5
+plt.figure()
+nx.draw(G, pos)
+plt.xlim(-0.05, 0.05)
+plt.ylim(-0.05, 0.05)
+plt.show()
 
-for node in G.nodes():
+ohmic_ratio = 0.1
+
+nodes = list(G.nodes())
+
+for node in nodes:
     if np.random.rand() < ohmic_ratio:
+        
         neighbors = list(G.neighbors(node))
+        
+        edges_to_add = itertools.combinations(neighbors, 2)
+        
+        G.add_edges_from(edges_to_add)
+        
+        G.remove_node(node)
+
+plt.figure()
+nx.draw(G, pos)
+plt.xlim(-0.05, 0.05)
+plt.ylim(-0.05, 0.05)
+plt.show()
