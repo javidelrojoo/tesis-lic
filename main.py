9edc60141b237170c7d0b062d54e1f1f469fd0df
javidelrojoo <javitau459@gmail.com>
2026-03-26 14:48:36 -0300

    Agrego los grafos de la muestra m1 y m4

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from SkeletonAnalysis import SkeletonAnalysis
+import matplotlib.pyplot as plt
 
-muestra = 'M3'
+muestra = 'M1'
 dir_path = './muestras/' + muestra + '/'
-img_path = dir_path + 'M30007_binary.tif'
-electrodes_path = (dir_path + 'M30007_electrode1.tif', dir_path + 'M30007_electrode2.tif')
+img_path = dir_path + muestra + '_binary.tif'
+electrodes_path = (dir_path + muestra + '_electrode_mask1.tif', dir_path + muestra + '_electrode_mask2.tif')
 
 SA = SkeletonAnalysis(img_path)
-
-SA.complete_analysis(plot=True, verbose=True, save_graph_path=dir_path + 'M30007.graphml', electrodes_path=electrodes_path)
+# SA.load_electrodes(*electrodes_path)
+# SA.skeletonize()
+# plt.figure(figsize=(8, 8))
+# plt.imshow(SA.img, cmap='grey')
+# plt.imshow(SA.skeleton_img, cmap='grey', alpha=0.5)
+# plt.axis('off')
+# plt.tight_layout()
+# # plt.savefig(dir_path + 'M3_skeleton.png', bbox_inches='tight', pad_inches=0, dpi=400)
+# plt.show()
+SA.complete_analysis(plot=False, verbose=True, save_graph_path=dir_path + muestra + '.graphml', electrodes_path=electrodes_path)
