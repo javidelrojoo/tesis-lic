e91f454d24ad89561e3ee111e4bba9bb76eed248
javidelrojoo <javitau459@gmail.com>
2026-05-04 11:54:25 -0300

    Aplico un procesamiento de las imagenes de electrodos para evitar islas de ruido no deseadas

--- SkeletonAnalysis.py
+++ SkeletonAnalysis.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
-from skimage.morphology import remove_small_holes, opening, disk, skeletonize, dilation
+from skimage.morphology import remove_small_holes, opening, disk, skeletonize, dilation, remove_small_objects
+from scipy.ndimage import binary_fill_holes
 from skan import Skeleton
 from skan.csr import skeleton_to_nx
 import networkx as nx
@@ -14,48 +15,54 @@ class SkeletonAnalysis:
     def load_electrodes(self, path1, path2):
         self.electrode_mask1 = cv2.imread(path1, cv2.IMREAD_GRAYSCALE)
         self.electrode_mask2 = cv2.imread(path2, cv2.IMREAD_GRAYSCALE)
+
+        self.electrode_mask1 = binary_fill_holes(self.electrode_mask1)
+        self.electrode_mask2 = binary_fill_holes(self.electrode_mask2)
+
+        self.electrode_mask1 = remove_small_objects(self.electrode_mask1, max_size=500)
+        self.electrode_mask2 = remove_small_objects(self.electrode_mask2, max_size=500)
+
         self.has_electrodes = True
     
     def img_cleaning(self):
-        self.img = remove_small_holes(self.img.astype(bool), area_threshold=2)
+        self.img = remove_small_holes(self.img.astype(bool), max_size=2)
         self.img = opening(self.img, footprint=disk(1))
     
     def skeletonize(self):
         self.skeleton_img = skeletonize(self.img)
         if self.has_electrodes:
+            # Solo eliminás los electrodos, sin agregar contact_points al skeleton
             self.skeleton_img = self.skeleton_img & ~self.electrode_mask1 & ~self.electrode_mask2
-            
-            electrode_dil1 = dilation(self.electrode_mask1, disk(5))
-            electrode_dil2 = dilation(self.electrode_mask2, disk(5))
 
-            self.contact_points1 = self.skeleton_img & electrode_dil1 & ~self.electrode_mask1 & ~self.electrode_mask2
-            self.contact_points2 = self.skeleton_img & electrode_dil2 & ~self.electrode_mask1 & ~self.electrode_mask2
-
-            self.skeleton_img = self.skeleton_img | self.contact_points1 | self.contact_points2
+            # Guardás las máscaras dilatadas para usarlas en get_network
+            self.electrode_dil1 = dilation(self.electrode_mask1, disk(5))
+            self.electrode_dil2 = dilation(self.electrode_mask2, disk(5))
     
     def get_network(self):
         skeleton = Skeleton(self.skeleton_img)
         self.G = skeleton_to_nx(skeleton)
         self.node_coords = np.array(skeleton.coordinates)
+
         if self.has_electrodes:
             electrode_nodes1 = []
             electrode_nodes2 = []
 
             for node_id, (y, x) in enumerate(self.node_coords):
-                if self.contact_points1[int(y), int(x)]:
+                yi, xi = int(y), int(x)
+                # Nodo está en la zona de contacto: dentro de la dilatación pero fuera del electrodo
+                if self.electrode_dil1[yi, xi] and not self.electrode_mask1[yi, xi] and not self.electrode_mask2[yi, xi]:
                     electrode_nodes1.append(node_id)
-                if self.contact_points2[int(y), int(x)]:
+                if self.electrode_dil2[yi, xi] and not self.electrode_mask2[yi, xi] and not self.electrode_mask1[yi, xi]:
                     electrode_nodes2.append(node_id)
 
-            self.G.add_node("Vin", pos=(2400, -1000))
-            self.G.add_node("Vout", pos=(3200, -4500))  
+            self.G.add_node("Vin")
+            self.G.add_node("Vout")
 
             for n in electrode_nodes1:
                 self.G.add_edge("Vin", n)
-
             for n in electrode_nodes2:
                 self.G.add_edge("Vout", n)
-    
+            
     def clean_network(self):
         changed = True
         while changed:
@@ -95,18 +102,15 @@ class SkeletonAnalysis:
         pos = {}
         
         if self.has_electrodes:
-            
-            _, binaria1 = cv2.threshold(self.electrode_mask1, 127, 255, cv2.THRESH_BINARY)
-            M1 = cv2.moments(binaria1)
+
+            M1 = cv2.moments(self.electrode_mask1.astype(np.uint8))
+            M2 = cv2.moments(self.electrode_mask2.astype(np.uint8))
             cx1 = int(M1["m10"] / M1["m00"])
             cy1 = int(M1["m01"] / M1["m00"])
-            
-            _, binaria2 = cv2.threshold(self.electrode_mask2, 127, 255, cv2.THRESH_BINARY)
-            M2 = cv2.moments(binaria2)
             cx2 = int(M2["m10"] / M2["m00"])
             cy2 = int(M2["m01"] / M2["m00"])
             
-        for i, n in enumerate(self.G.nodes):
+        for n in self.G.nodes:
             if n == "Vin":
                 pos[n] = (cx1, cy1)
             elif n == "Vout":
@@ -114,16 +118,17 @@ class SkeletonAnalysis:
             else:
                 y, x = self.node_coords[n]
                 pos[n] = (x, y)
-        
-        nodos_electrodos = [n for n in self.G.nodes if n in ["Vin", "Vout"]]
-        nodos_red = [n for n in self.G.nodes if n not in ["Vin", "Vout"]]
-        
+                
         plt.figure(figsize=(8, 8))
-        
+        plt.imshow(self.img, cmap='gray')  # Primero la imagen
+
+        nodos_electrodos = [n for n in self.G.nodes if n in ("Vin", "Vout")]
+        nodos_red = [n for n in self.G.nodes if n not in ("Vin", "Vout")]
+
+
         nx.draw_networkx_edges(self.G, pos, width=1.5, edge_color='green', node_size=25)
         nx.draw_networkx_nodes(self.G, pos, nodelist=nodos_red, node_size=25, node_color='#1f78b4')
         nx.draw_networkx_nodes(self.G, pos, nodelist=nodos_electrodos, node_size=150, node_color='red')
-        plt.imshow(self.img, cmap='gray')
         plt.axis('off')
         if save_path:
             plt.savefig(save_path, bbox_inches='tight', pad_inches=0, dpi=400)
